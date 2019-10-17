8c1730bbd2c2c21a4c525bd9a7ebd428dd0f707c
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2019-10-17 08:21:30 -0300

    Update Integração_Monte_Carlo.py

--- Integração_Monte_Carlo.py
+++ Integração_Monte_Carlo.py
@@ -10,42 +10,26 @@ Created on Thu Oct 17 06:14:52 2019
 import math
 import numpy
 
-random_results = []
-random_numb = 10000
-var_count = 0
-
-def function(x):
+def g(x):
 
 	return(math.exp(-3*x)*math.sin(4*x))
 
-def intervalos():
-	limites = []
-	print("digite o intervalo de busca [x0,x1]:")
-	for x in range(0,2):
-		if(x == 0):
-			y = float(input("digite o limite esquerdo x0: "))
-			limites.append(y)
-		else:
-			y = float(input("digite o limite direito x1: "))
-			limites.append(y)
+def monte_carlo(g,x0,x1):
+	random_results = []
+	random_numb = 1000
+	var_count = 0
 
-	return(limites)
+	while(var_count<100):
+		SOMA1 = 0
+		for x in range(random_numb):
 
-I = intervalos()
-
-while(var_count<1000):
-	SOMA1 = 0
-	for x in range(random_numb):
-
-		SOMA1 += function(I[0]+(I[1]-I[0])*numpy.random.uniform(0,1,1))
+			SOMA1 += g(x0+(x1-x0)*numpy.random.uniform(0,1,1))
 	
-	random_approximation = float(((I[1]-I[0])/random_numb)*SOMA1)
-	random_results.append(random_approximation)
-	var_count = var_count + 1
+		random_approximation = float(((x1-x0)/random_numb)*SOMA1)
+		random_results.append(random_approximation)
+		var_count = var_count + 1
 
 
-Integral = float(numpy.mean(random_results))
-
-print("\n")
-print("A Integral Aproximada da Função é:%.8f" %(Integral))
-
+	Integral = float(numpy.mean(random_results))
+	print("\n")
+	return(print("\tA Integral Aproximada da Função é:%.8f" %(Integral)))
