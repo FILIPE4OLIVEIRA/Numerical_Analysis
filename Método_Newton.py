09697d99a1f1153df738f99ef8fb294784150ebd
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2019-10-17 08:52:33 -0300

    Update Método_Newton.py

--- Método_Newton.py
+++ Método_Newton.py
@@ -9,37 +9,33 @@ Created on Mon Sep 30 22:16:09 2019
 
 import math
 
-erro = 0.0000001
-maxint = 100
-var_count = 1
-
-def function(x):
+def G(x):
 
     return(math.exp(-3*x)*math.sin(4*x))
 
-def diffunction(x):
+def g(x):
     
     return(math.exp(-3*x)*(4*math.cos(4*x)-3*math.sin(4*x)))
-    
-x0 = float(input("digite o chute inicial x0: "))
+ 
+def newton(G,g,x0):
+    erro = 0.0000001
+    maxint = 100
+    var_count = 1
 
-print("\n")
-if(abs(function(x0))<erro):
-	print("A raiz aproximada da função é: %.8f"%(x0))
-else:
-    print("Iteração\t Ponto (x)\t f(x)")
-    while(var_count<maxint and abs(function(x0))>erro):
-    	
-    	x1 = x0 - (function(x0)/diffunction(x0))
-
-    	print("%d\t\t %.8f\t %.8f" %(var_count,x1,abs(function(x1))))
-
-    	x0 = x1
-    	var_count = var_count + 1
-
-    if(var_count>maxint):
-    	print("\n")
-    	print("ERRO: Numero Máximo de Iterações Atingido!")
+    if(abs(G(x0))<erro):
+        print("\n")
+        print("A raiz aproximada da função é: %.8f"%(x0))
     else:
-    	print("\n")
-    	print("A raiz aproximada da função é: %.8f" %(x0))
+        print("Iteração\t Ponto(x1)\t |G(x1)|")
+        while(var_count<maxint and abs(G(x0))>erro):
+            x1 = x0 - (G(x0)/g(x0))
+            print("%d\t\t %.8f\t %.8f" %(var_count,x1,abs(G(x1))))
+            x0 = x1
+            var_count = var_count + 1
+
+        if(var_count>maxint):
+            print("\n")
+            print("ERRO: Numero Máximo de Iterações Atingido!")
+        else:
+            print("\n")
+            print("A raiz aproximada da função é: %.8f" %(x0))
