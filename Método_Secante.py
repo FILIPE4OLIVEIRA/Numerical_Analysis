08e24250565e829a40792773fa2b100a6ab32889
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2019-10-17 08:44:09 -0300

    Update Método_Secante.py

--- Método_Secante.py
+++ Método_Secante.py
@@ -9,52 +9,37 @@ Created on Sat Oct  5 20:46:37 2019
 
 import math
 
-erro = 0.0000001
-maxint = 100
-var_count = 1
-
-def function(x):
+def g(x):
 
 	return(math.exp(-3*x)*math.sin(4*x))
 
-def intervalos():
-	limites = []
-	print("digite o intervalo de busca [x0,x1]:")
-	for x in range(0,2):
-		if(x == 0):
-			y = float(input("digite o limite esquerdo x0: "))
-			limites.append(y)
-		else:
-			y = float(input("digite o limite direito x1: "))
-			limites.append(y)
-
-	return(limites)
-
-I = intervalos()
-
-print("\n")
-if(function(I[0])*function(I[1])<0):
-	x2 = (I[0]+I[1])/2
-	print("Iteração\t Ponto (x0)\t Ponto (x1)\t f(x2)")
-	while(var_count<maxint and abs(function(x2))>erro):
-
-		x2 = I[1] - ((function(I[1])*(I[1]-I[0]))/(function(I[1])- function(I[0])))
-
-		print("%d\t\t %.8f\t %.8f\t %.8f" %(var_count,I[0],I[1],abs(function(x2))))
-
-		I[0] = I[1]
-
-		I[1] = x2
-
-		var_count = var_count + 1
-		
-	if(var_count>maxint):
+def secante(g,x0,x1):
+	erro = 0.0000001
+	maxint = 100
+	var_count = 1
+	if(g(x0)*g(x1)<0):
+		x2 = (x0+x1)/2
 		print("\n")
-		print("ERRO: Numero Máximo de Iterações Atingido!")
+		print("Iteração\t Ponto(x0)\t Ponto(x1)\t |g(x2)|")
+		while(var_count<maxint and abs(g(x2))>erro):
+
+			x2 = x1 - ((g(x1)*(x1-x0))/(g(x1)-g(x0)))
+
+			print("%d\t\t %.8f\t %.8f\t %.8f" %(var_count,x0,x1,abs(g(x2))))
+
+			x0 = x1
+
+			x1 = x2
+
+			var_count = var_count + 1
+		
+		if(var_count>maxint):
+			print("\n")
+			print("ERRO: Numero Máximo de Iterações Atingido!")
+		else:
+			print("\n")
+			print("A raiz aproximada da função é: %.8f" %(x2))
+
 	else:
 		print("\n")
-		print("A raiz aproximada da função é: %.8f" %(x2))
-
-else:
-	print("\n")
-	print("Não existe raiz nesse intervalo.")
+		print("Não existe raiz nesse intervalo.")
