ba2d6d0043a363ba964a62ff5afa3e8e1bc5f458
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2019-11-03 04:16:16 -0300

    Update Integral_Dupla_Monte_Carlo.py

--- Integral_Dupla_Monte_Carlo.py
+++ Integral_Dupla_Monte_Carlo.py
@@ -47,7 +47,7 @@ def integral_dupla(g,x0,x1,y0,y1):
 	graph1 = Axes3D(figura1)
 	graph1.plot_surface(x, y, z, rstride=3, cstride=3, cmap=cm.viridis)
 
-	plt.title('Gráfico de g(x,y)')
+	plt.title('Gráfico de G(x,y)')
 	graph1.set_xlabel('EIXO X')
 	graph1.set_ylabel('EIXO Y')
 	graph1.set_zlabel('EIXO Z')
@@ -56,7 +56,7 @@ def integral_dupla(g,x0,x1,y0,y1):
 	graph2 = figura2.add_subplot(111)
 	graph2.contourf(x,y,z,cmap=cm.viridis)
 
-	plt.title('Gráfico de g(x,y)')
+	plt.title('Gráfico de G(x,y)')
 	graph2.set_xlabel('EIXO X')
 	graph2.set_ylabel('EIXO Y')
 	
