f040afeca031b26b7b7baea9b34628a93f0c31e5
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2019-11-03 01:54:03 -0300

    Update Integração_Trapezio.py

--- Integração_Trapezio.py
+++ Integração_Trapezio.py
@@ -7,11 +7,12 @@ Created on Sun Oct 13 05:56:53 2019
 
 # Regra do Trapezio Composta
 
-import math
+import numpy
+import matplotlib.pyplot as plt
 
 def g(x):
 
-	return(math.exp(-3*x)*math.sin(4*x))
+	return(numpy.exp(-3*x)*numpy.sin(4*x))
 
 def trapezio(g,x0,x1):
 	numb_breaks = 1000
@@ -23,6 +24,17 @@ def trapezio(g,x0,x1):
 		SOMA1 += 2*g(x0 + x*step)
 
 	Integral = float((step/2)*(g(x0) + SOMA1 + g(x1)))
+
+	#Plotagem do Gráfico de G(x)
+	x = numpy.linspace(x0,x1)
+
+	plt.title('Gráfico de g(x)')
+	plt.xlabel('EIXO X')
+	plt.ylabel('EIXO Y')
+	plt.plot(x,g(x))
+	plt.fill_between(x,g(x))
+	plt.grid()
+	plt.show()
 
 	print("\n")
 	return(print("\tA Integral Aproximada da Função é:%.8f" %(Integral)))
