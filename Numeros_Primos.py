06741660381844f7b9973c7fa6a6fbb551e02da2
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-02-06 16:22:57 -0300

    Update Numeros_Primos.py

--- Numeros_Primos.py
+++ Numeros_Primos.py
@@ -9,26 +9,27 @@ Created on Sat Sep 28 22:38:09 2019
 
 numb = int(input("digite um número limite:"))
 
-odd_numb = []					#impares
-even_numb = []					#pares
-prime_numb = [2]				#primos
+odd_numb = []  # impares
+even_numb = []  # pares
+prime_numb = [2]  # primos
 
-for x in range(2,numb+1):               	# Cria um lista de números Pares e Impares
-	if (x%2 == 0):
-		even_numb.append(x)
-	else:
-		odd_numb.append(x)
+for x in range(2, numb + 1):  # Cria um lista de números Pares e Impares
+    if (x % 2 == 0):
+        even_numb.append(x)
+    else:
+        odd_numb.append(x)
 
-for x in range(2,len(odd_numb)+1):      
-	for y in range(3,len(odd_numb)+1):  	# Remove todos os múltiplos impares da lista
-		z = y*x	
-		if (z in odd_numb):
-			odd_numb.remove(z)
+for x in range(2, len(odd_numb) + 1):
+    for y in range(3, len(odd_numb) + 1):  # Remove todos os múltiplos impares da lista
+    	z = y * x
+    	if (z in odd_numb):
+        	odd_numb.remove(z)
 
-for x in odd_numb:                      	# Monta o vetor contendo todos os números com resto da divisão igual a zero
-	for y in range(3,numb+1):
-		if (x%y == 0):
-			prime_numb.append(x)
+for x in odd_numb:  # Monta o vetor contendo todos os números com resto da divisão igual a zero
+    for y in range(3, numb + 1):
+        if (x % y == 0):
+            prime_numb.append(x)
 
-print("A lista de numeros primos é:")
+print("\n São %.f Números Primos" % (len(prime_numb)))
+print("\n A lista de numeros primos é:")
 print(prime_numb)
