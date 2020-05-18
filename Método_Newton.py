0473aa2a452ed761d47398fa561ea260e0752cfb
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-05-17 21:58:00 -0300

    Update Método_Newton.py

--- Método_Newton.py
+++ Método_Newton.py
@@ -8,30 +8,30 @@ Created on Mon Sep 30 22:16:09 2019
 # Método Numérico de Newton
 
 import numpy
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 
-def G(x):
+def y(x):
 
     return(numpy.exp(-3*x)*numpy.sin(4*x))
 
-def g(x):
+def dydx(x):
     
     return(numpy.exp(-3*x)*(4*numpy.cos(4*x)-3*numpy.sin(4*x)))
  
-def newton(G,g,x0):
+def Newton(y,dydx,x0):
     erro = 0.0000001
     maxint = 100
     var_count = 1
     
     #Método Numérico de Newton   
-    if(abs(G(x0))<erro):
+    if(abs(y(x0))<erro):
         print("\n")
         print("A raiz aproximada da função é: %.8f"%(x0))
     else:
         print("Iteração\t Ponto(x1)\t |G(x1)|")
-        while(var_count<maxint and abs(G(x0))>erro):
-            x1 = x0 - (G(x0)/g(x0))
-            print("%d\t\t %.8f\t %.8f" %(var_count,x1,abs(G(x1))))
+        while(var_count<maxint and abs(y(x0))>erro):
+            x1 = x0 - (y(x0)/dydx(x0))
+            print("%d\t\t %.8f\t %.8f" %(var_count,x1,abs(y(x1))))
             x0 = x1
             var_count = var_count + 1
 
@@ -43,12 +43,12 @@ def newton(G,g,x0):
             print("A raiz aproximada da função é: %.8f" %(x0))
     
     #Plotagem do Gráfico de G(x)
-    x = numpy.linspace(0.95*x0,1.15*x0)
+    xt = numpy.linspace(0.80*x0,1.20*x0)
 
-    plt.title('Gráfico de g(x)')
-    plt.xlabel('EIXO X')
-    plt.ylabel('EIXO Y')
-    plt.plot(x,G(x))
-    plt.fill_between(x,G(x))
-    plt.grid()
-    plt.show()
+    pyplot.title('Gráfico de y(x)')
+    pyplot.xlabel('EIXO X')
+    pyplot.ylabel('EIXO Y')
+    pyplot.plot(xt,y(xt), color = "red")
+    pyplot.fill_between(xt,y(xt), color = "gray")
+    pyplot.grid()
+    pyplot.show()
