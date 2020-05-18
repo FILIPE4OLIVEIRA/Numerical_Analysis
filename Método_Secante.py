bbd8554feb807ba71fc71e5d2dd6f82d53e6c4d9
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-05-18 04:03:41 -0300

    Update Método_Secante.py

--- Método_Secante.py
+++ Método_Secante.py
@@ -10,23 +10,23 @@ Created on Sat Oct  5 20:46:37 2019
 import numpy
 import matplotlib.pyplot as pyplot
 
-def g(x):
+def y(x):
 
 	return(numpy.exp(-3*x)*numpy.sin(4*x))
 
-def Secante(g,x0,x1):
+def Secante(y,x0,x1):
 	erro = 0.0000001
 	maxint = 100
 	var_count = 1
 
 	#Método Numérico da Secante
-	if(g(x0)*g(x1)<0):
+	if(y(x0)*y(x1)<0):
 		x2 = (x0+x1)/2
 		print("\n")
 		print("Iteração\t Ponto(x0)\t Ponto(x1)\t |g(x2)|")
-		while(var_count<maxint and abs(g(x2))>erro):
-			x2 = x1 - ((g(x1)*(x1-x0))/(g(x1)-g(x0)))
-			print("%d\t\t %.8f\t %.8f\t %.8f" %(var_count,x0,x1,abs(g(x2))))
+		while(var_count<maxint and abs(y(x2))>erro):
+			x2 = x1 - ((y(x1)*(x1-x0))/(y(x1)-y(x0)))
+			print("%d\t\t %.8f\t %.8f\t %.8f" %(var_count,x0,x1,abs(y(x2))))
 			x0 = x1
 			x1 = x2
 			var_count = var_count + 1
@@ -45,10 +45,11 @@ def Secante(g,x0,x1):
 	#Plotagem do Gráfico de G(x)
 	xt = numpy.linspace(0.80*x2,1.20*x2)
 
-	pyplot.title('Gráfico de g(x)')
+	pyplot.title('Gráfico de y(x)')
 	pyplot.xlabel('EIXO X')
 	pyplot.ylabel('EIXO Y')
-	pyplot.plot(xt,g(xt), color = "red")
-	pyplot.fill_between(xt,g(xt), color = "gray")
+	pyplot.plot(xt,y(xt), color = "red", label = "y(x) = e^(-3x)sin(4x)")
+	pyplot.fill_between(xt,y(xt), color = "gray")
+	pyplot.legend(loc = 'best')
 	pyplot.grid()
 	pyplot.show()
