64075bbd7fc86c46d56b8e545c595ca154952e3c
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-05-17 21:48:28 -0300

    Update Integral_Simples_Monte_Carlo.py

--- Integral_Simples_Monte_Carlo.py
+++ Integral_Simples_Monte_Carlo.py
@@ -8,18 +8,18 @@ Created on Thu Oct 17 06:14:52 2019
 # Integração de Monte Carlo
 
 import numpy
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 
 def g(x):
 
 	return(1/2 + x*numpy.exp(-x**2))
 
-def integral_simples(g,x0,x1):
+def Integral_Simples(g,x0,x1):
 	random_results = []
-	random_numb = 1000
+	random_numb = 100
 	var_count = 0
 
-	while(var_count<10000):
+	while(var_count<150):
 		SOMA1 = 0
 		for x in range(random_numb):
 			X = x0+(x1-x0)*numpy.random.uniform(0,1,1)
@@ -32,26 +32,28 @@ def integral_simples(g,x0,x1):
 
 	Integral = float(numpy.mean(random_results))
     
-    	#Plotagem do Gráfico de G(x)
-	x = numpy.linspace(x0,x1)
+    #Plotagem do Gráfico de G(x)
+	xt = numpy.linspace(x0,x1)
 
 	#Plotagem do Gráfico 1
-	plt.figure(1)
-	plt.title('Gráfico de Área: G(x)')
-	plt.xlabel('EIXO X')
-	plt.ylabel('EIXO Y')
-	plt.plot(x,g(x))
-	plt.fill_between(x,g(x))
-	plt.grid()
-	plt.show()
+	pyplot.title('Gráfico de g(x)')
+	pyplot.xlabel('EIXO X')
+	pyplot.ylabel('EIXO Y')
+	pyplot.plot(xt,g(xt), color = "red", label = "g(x) = 1/2 + xe^(-x²)")
+	pyplot.fill_between(xt,g(xt), color = "gray")
+	pyplot.text(x = x1*0.30, y = x1*0.10, s = "Área Aproximada = %.8f" %(Integral))
+	pyplot.legend(loc = 'upper left')
+	pyplot.grid()
+	pyplot.show()
 
 	#Plotagem do Gráfico 2
-	plt.figure(2)
-	plt.title('Distribuição Normal da Integral')
-	plt.hist(random_results, bins = 30, ec = 'black')
-	plt.xlabel('Valor Médio da Integral')
-	plt.ylabel('Frequência')
-	plt.show()
+	pyplot.figure(2)
+	pyplot.title('Distribuição Normal dos Resultados da Integral')
+	pyplot.hist(random_results, bins = 30, ec = 'black')
+	pyplot.xlabel('Valor Médio da Integral')
+	pyplot.ylabel('Frequência')
+	pyplot.grid()
+	pyplot.show()
     
 	
 	print("\n")
