b3cf969b8b59c569915d111edb19b53a42d5a743
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-05-17 21:43:45 -0300

    Update Classe_Métodos_Integração.py

--- Classe_Métodos_Integração.py
+++ Classe_Métodos_Integração.py
@@ -8,17 +8,17 @@ Created on Tue Fev 10 00:31:15 2020
 # Métodos Numéricos para Solução de Integrais
 
 import numpy
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 
-class Integração:
+class Integração():
 
-    def __init__(self,x0_inicial = -3,x1_final = 3,função_inicial = lambda x: 1/2 + x*numpy.exp(-x**2)):
-
+    def __init__(self,x0_inicial = -3,x1_final = 3,função_inicial = '1/2 + x*numpy.exp(-x**2)'):
         self.x0_inicial     = x0_inicial
         self.x1_final       = x1_final
-        self.função         = função_inicial
+        self.função         = lambda x: eval(função_inicial)
+        self.função_label   = str(função_inicial)
 
-    def regra_trapezio(self):
+    def Regra_Trapezio(self):
         numb_breaks = 100000
         SOMA1 = 0
         step = (self.x1_final - self.x0_inicial) / numb_breaks
@@ -32,18 +32,20 @@ class Integração:
         # Plotagem do Gráfico de G(x)
         x = numpy.linspace(self.x0_inicial,self.x1_final)
 
-        plt.title('Gráfico de g(x)')
-        plt.xlabel('EIXO X')
-        plt.ylabel('EIXO Y')
-        plt.plot(x, self.função(x))
-        plt.fill_between(x, self.função(x))
-        plt.grid()
-        plt.show()
+        pyplot.title('Gráfico de g(x)')
+        pyplot.xlabel('EIXO X')
+        pyplot.ylabel('EIXO Y')
+        pyplot.plot(x, self.função(x), color = "red", label = self.função_label)
+        pyplot.fill_between(x, self.função(x), color = "gray")
+        pyplot.text(x = self.x1_final*0.30, y = self.x1_final*0.10, s = "Área Aproximada = %.8f" %(Integral))
+        pyplot.legend(loc = 'upper left')
+        pyplot.grid()
+        pyplot.show()
 
         print("\n")
         return(print("\tA Integral Aproximada da Função é: %.8f" % (Integral)))
 
-    def regra_simpson(self):
+    def Regra_Simpson(self):
         numb_breaks = 100000
         SOMA1 = 0
         SOMA2 = 0
@@ -63,19 +65,21 @@ class Integração:
         #Plotagem do Gráfico de G(x)
         x = numpy.linspace(self.x0_inicial,self.x1_final)
 
-        plt.title('Gráfico de g(x)')
-        plt.xlabel('EIXO X')
-        plt.ylabel('EIXO Y')
-        plt.plot(x,self.função(x))
-        plt.fill_between(x,self.função(x))
-        plt.grid()
-        plt.show()
+        pyplot.title('Gráfico de g(x)')
+        pyplot.xlabel('EIXO X')
+        pyplot.ylabel('EIXO Y')
+        pyplot.plot(x, self.função(x), color = "red", label = self.função_label)
+        pyplot.fill_between(x, self.função(x), color = "gray")
+        pyplot.text(x = self.x1_final*0.30, y = self.x1_final*0.10, s = "Área Aproximada = %.8f" %(Integral))
+        pyplot.legend(loc = 'upper left')
+        pyplot.grid()
+        pyplot.show()
     
         print("\n")
         return(print("\tA Integral Aproximada da Função é: %.8f" %(Integral)))
 
 
-    def regra_intervalo(self):
+    def Regra_Intervalo(self):
         numb_breaks = 100000
         SOMA1 = 0
         step = (self.x1_final - self.x0_inicial) / numb_breaks
@@ -90,13 +94,15 @@ class Integração:
         #Plotagem do Gráfico de G(x)
         x = numpy.linspace(self.x0_inicial,self.x1_final)
 
-        plt.title('Gráfico de g(x)')
-        plt.xlabel('EIXO X')
-        plt.ylabel('EIXO Y')
-        plt.plot(x,self.função(x))
-        plt.fill_between(x,self.função(x))
-        plt.grid()
-        plt.show()
+        pyplot.title('Gráfico de g(x)')
+        pyplot.xlabel('EIXO X')
+        pyplot.ylabel('EIXO Y')
+        pyplot.plot(x, self.função(x), color = "red", label = self.função_label)
+        pyplot.fill_between(x, self.função(x), color = "gray")
+        pyplot.text(x = self.x1_final*0.30, y = self.x1_final*0.10, s = "Área Aproximada = %.8f" %(Integral))
+        pyplot.legend(loc = 'upper left')
+        pyplot.grid()
+        pyplot.show()
 
         print("\n")
         return(print("\tA Integral Aproximada da Função é: %.8f" %(Integral)))
