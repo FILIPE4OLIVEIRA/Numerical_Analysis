7cbb8fa347b150cf0e678a79fa882976f01cdb12
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-05-17 21:53:10 -0300

    Update Integração_Trapezio.py

--- Integração_Trapezio.py
+++ Integração_Trapezio.py
@@ -8,33 +8,37 @@ Created on Sun Oct 13 05:56:53 2019
 # Regra do Trapezio Composta
 
 import numpy
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
+
 
 def g(x):
 
-	return(numpy.exp(-3*x)*numpy.sin(4*x))
+    return(1/2 + x*numpy.exp(-x**2))
 
-def trapezio(g,x0,x1):
-	numb_breaks = 1000
-	SOMA1 = 0
-	step = (x1-x0)/numb_breaks
 
-	for x in range(1,numb_breaks):
+def Trapezio(g, x0, x1):
+    numb_breaks = 1000
+    SOMA1 = 0
+    step = (x1 - x0) / numb_breaks
 
-		SOMA1 += 2*g(x0 + x*step)
+    for x in range(1, numb_breaks):
 
-	Integral = float((step/2)*(g(x0) + SOMA1 + g(x1)))
+        SOMA1 += 2*g(x0 + x*step)
 
-	#Plotagem do Gráfico de G(x)
-	x = numpy.linspace(x0,x1)
+    Integral = float((step/2)*(g(x0) + SOMA1 + g(x1)))
 
-	plt.title('Gráfico de g(x)')
-	plt.xlabel('EIXO X')
-	plt.ylabel('EIXO Y')
-	plt.plot(x,g(x))
-	plt.fill_between(x,g(x))
-	plt.grid()
-	plt.show()
+    #Plotagem do Gráfico de G(x)
+    xt = numpy.linspace(x0,x1)
 
-	print("\n")
-	return(print("\tA Integral Aproximada da Função é: %.8f" %(Integral)))
+    pyplot.title('Gráfico de g(x)')
+    pyplot.xlabel('EIXO X')
+    pyplot.ylabel('EIXO Y')
+    pyplot.plot(xt,g(xt), color = "red", label = "g(x) = 1/2 + xe^(-x²)")
+    pyplot.fill_between(xt,g(xt), color = "gray")
+    pyplot.text(x = x1*0.30, y = x1*0.10, s = "Área Aproximada = %.8f" %(Integral))
+    pyplot.legend(loc = 'upper left')
+    pyplot.grid()
+    pyplot.show()
+
+    print("\n")
+    return(print("\tA Integral Aproximada da Função é: %.8f" % (Integral)))
