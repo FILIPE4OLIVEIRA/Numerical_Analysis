a998c3cb6446d5acfc4b784cba3143e9b15c640f
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-06-10 15:36:45 -0300

    Update EDO_Método_RungeKutta4.py

--- EDO_Método_RungeKutta4.py
+++ EDO_Método_RungeKutta4.py
@@ -4,8 +4,8 @@ Created on Wed Jan  1 19:44:36 2020
 @author: engoliveira
 
 """
-
-# Este método resolve o problema da EDO y' - 2xy = 0 ; y(1) = 1 
+# EDO: y' + P(x)y = 0
+# Este método resolve o problema da EDO y' - 2xy = 0 ; y(0) = 1 
 
 # Método de Runge Kutta 4° Ordem
 
@@ -13,41 +13,38 @@ import numpy
 import matplotlib.pyplot as pyplot
 
 # Função
-def g(x,y):
-
+def w(x,y):
 	return (2*x*y)
 
-# Método de Runge Kutta
-def Runge_Kutta_O4(g,x0,x1,y0):
+# Método de Runge Kuttas
+def Runge_Kutta_O4(w,x0,x1,y0):
 
 	numb_div = 10000
-	numb_int = (x1-x0)/numb_div
+	h = (x1-x0)/numb_div
 
 	x_aprox = [x0]
 	y_aprox = [y0]
 
-	for i in range(0,numb_div):
+	for i in range(numb_div):
 
-		k1 = g(x_aprox[i],y_aprox[i])
-		k2 = g(x_aprox[i] + (numb_int/2),y_aprox[i] + k1*(numb_int/2))
-		k3 = g(x_aprox[i] + (numb_int/2),y_aprox[i] + k2*(numb_int/2))
-		k4 = g(x_aprox[i] + numb_int,y_aprox[i] + k3*numb_int)
+		k1 = h*w(x_aprox[i], y_aprox[i])
+		k2 = h*w(x_aprox[i] + (h/2), y_aprox[i] + k1*(h/2))
+		k3 = h*w(x_aprox[i] + (h/2), y_aprox[i] + k2*(h/2))
+		k4 = h*w(x_aprox[i] + h, y_aprox[i] + k3)
 
-		K = (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+		y_next = y_aprox[i] + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+		x_next = x_aprox[i] + h
 
-		y1 = y_aprox[i] + numb_int*K
-		x1 = x_aprox[i] + numb_int
-		y_aprox.append(y1)
-		x_aprox.append(x1)
+		y_aprox.append(y_next)
+		x_aprox.append(x_next)
 
-	print("\n\t O Valor Aproximado da EDO no Ponto %.2f é: %.8f" %(x1,y_aprox[-1]))
+	print("\nA função y(x) no ponto %.2f é aproximadamente: %.8f" %(x1,y_aprox[-1]))
 
 	# Plotagem do Gráfico 1
-	pyplot.figure(1)
-	pyplot.title("Gráfico da EDO \n y' - 2xy = 0 ; y(1) = 1")
+	pyplot.title("Gráfico da EDO \n y' - 2xy = 0 ; y(0) = 1")
 	pyplot.xlabel('EIXO X')
 	pyplot.ylabel('EIXO Y')
-	pyplot.plot(x_aprox,y_aprox, label = 'Solução', color = 'blue', linestyle = 'dashed')
-	pyplot.legend()
-	pyplot.grid()
+	pyplot.plot(x_aprox,y_aprox, label = 'Solução Númerica y(x)', color = 'blue', linestyle = 'dashed')
+	pyplot.legend(loc = 'best')
+	pyplot.grid(linestyle = '--', linewidth = 0.5)
 	pyplot.show()
