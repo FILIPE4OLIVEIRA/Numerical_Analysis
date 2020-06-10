eb881b7133c3bfa2e1e1321868eada328345e6bb
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-06-10 15:48:29 -0300

    Update EDO_Método_Euler.py

--- EDO_Método_Euler.py
+++ EDO_Método_Euler.py
@@ -4,8 +4,8 @@ Created on Wed Jan  1 18:49:35 2020
 @author: engoliveira
 
 """
-
-# Este método resolve o problema da EDO y' - 2xy = 0 ; y(1) = 1 
+# EDO: y' + P(x)y = 0
+# Este método resolve o problema da EDO y' - 2xy = 0 ; y(0) = 1  
 
 # Método de Euler - EDO
 
@@ -13,34 +13,34 @@ import numpy
 import matplotlib.pyplot as pyplot
 
 # Função
-def g(x,y):
+def P(x,y):
 
 	return (2*x*y)
 
 # Método de Euler
-def Euler_Method(g,x0,x1,y0):
+def Euler_Method(P,x0,x1,y0):
 
-	numb_div = 100000
-	numb_int = (x1-x0)/numb_div
+	numb_div = 500000
+	h = (x1-x0)/numb_div
 
 	x_aprox = [x0]
 	y_aprox = [y0]
 
-	for i in range(0,numb_div):
+	for i in range(numb_div):
 
-		y1 = y_aprox[i] + numb_int*g(x_aprox[i],y_aprox[i])
-		x1 = x_aprox[i] + numb_int
-		y_aprox.append(y1)
-		x_aprox.append(x1)
+		y_next = y_aprox[i] + h*P(x_aprox[i],y_aprox[i])
+		x_next = x_aprox[i] + h
 
-	print("\n\t O Valor Aproximado da EDO no Ponto %.2f é: %.8f" %(x1,y_aprox[-1]))
+		y_aprox.append(y_next)
+		x_aprox.append(x_next)
+
+	print("\nA função y(x) no ponto %.2f é aproximadamente: %.8f" %(x1,y_aprox[-1]))
 
 	# Plotagem do Gráfico 1
-	pyplot.figure(1)
-	pyplot.title("Gráfico da Solução: dy/dx - 2xy = 0 com y(1) = 1")
+	pyplot.title("Gráfico da Solução:\n y' - 2xy = 0 ; y(0) = 1")
 	pyplot.xlabel('EIXO X')
 	pyplot.ylabel('EIXO Y')
 	pyplot.plot(x_aprox,y_aprox, label = 'Solução', color = 'red', linestyle = 'dashed')
-	pyplot.legend()
-	pyplot.grid()
+	pyplot.legend(loc = 'best')
+	pyplot.grid(linestyle = '--', linewidth = 0.5)
 	pyplot.show()
