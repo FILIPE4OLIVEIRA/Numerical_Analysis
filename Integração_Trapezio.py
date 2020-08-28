9126823df1974eddca280a69a2aa1f83454055de
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-08-28 19:36:28 -0300

    Update Integração_Trapezio.py

--- Integração_Trapezio.py
+++ Integração_Trapezio.py
@@ -13,11 +13,12 @@ import matplotlib.pyplot as pyplot
 
 def g(x):
 
-    return(1/2 + x*numpy.exp(-x**2))
+    return(1/2 + 3*x*numpy.exp(-x**2))
 
 
-def Trapezio(g, x0, x1):
-    numb_breaks = 1000
+def Trapezio(g, x0=-2, x1=2):
+    
+    numb_breaks = 100000 # número de divisões do intervalo de integração
     SOMA1 = 0
     step = (x1 - x0) / numb_breaks
 
@@ -28,16 +29,15 @@ def Trapezio(g, x0, x1):
     Integral = float((step/2)*(g(x0) + SOMA1 + g(x1)))
 
     #Plotagem do Gráfico de G(x)
-    xt = numpy.linspace(x0,x1)
+    xt = numpy.linspace(x0,x1,1000)
 
     pyplot.title('Gráfico de g(x)')
-    pyplot.xlabel('EIXO X')
-    pyplot.ylabel('EIXO Y')
-    pyplot.plot(xt,g(xt), color = "red", label = "g(x) = 1/2 + xe^(-x²)")
-    pyplot.fill_between(xt,g(xt), color = "gray")
-    pyplot.text(x = x1*0.30, y = x1*0.10, s = "Área Aproximada = %.8f" %(Integral))
+    pyplot.plot(xt,g(xt), color = "black", label = "$g(x) = 1/2 + 3xe^{-x^2}$")
+    pyplot.fill_between(xt,g(xt), where = g(xt) >= 0, color = "green", interpolate = True, alpha = 0.3 , hatch = "///")
+    pyplot.fill_between(xt,g(xt), where = g(xt) <= 0, color = "red", interpolate = True,  alpha = 0.3 , hatch = "///")
+    pyplot.text(x = -2.0, y = 1.02, s = "Área Aproximada = %.8f" %(Integral))
     pyplot.legend(loc = 'upper left')
-    pyplot.grid()
+    pyplot.grid(linestyle = '--', linewidth = 0.5)
     pyplot.show()
 
     print("\n")
