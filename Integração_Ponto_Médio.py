ea7966fbc8e5101505280eb9680baef5a2a18f71
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-08-28 19:35:49 -0300

    Update Integração_Ponto_Médio.py

--- Integração_Ponto_Médio.py
+++ Integração_Ponto_Médio.py
@@ -12,10 +12,11 @@ import matplotlib.pyplot as pyplot
 
 def g(x):
 
-	return(1/2 + x*numpy.exp(-x**2))
+	return(1/2 + 3*x*numpy.exp(-x**2))
 
-def Ponto_Medio(g,x0,x1):
-	numb_breaks = 1000
+def Ponto_Medio(g,x0=-2,x1=2):
+
+	numb_breaks = 100000
 	SOMA1 = 0
 	step = (x1-x0)/numb_breaks
 
@@ -25,19 +26,17 @@ def Ponto_Medio(g,x0,x1):
 
 	Integral = float(2*step*SOMA1)
 
-	
-    #Plotagem do Gráfico de G(x)
-	xt = numpy.linspace(x0,x1)
+	#Plotagem do Gráfico de G(x)
+	xt = numpy.linspace(x0,x1,1000)
 
 	pyplot.title('Gráfico de g(x)')
-	pyplot.xlabel('EIXO X')
-	pyplot.ylabel('EIXO Y')
-	pyplot.plot(xt,g(xt), color = "red", label = "g(x) = 1/2 + xe^(-x²)")
-	pyplot.fill_between(xt,g(xt), color = "gray")
-	pyplot.text(x = x1*0.30, y = x1*0.10, s = "Área Aproximada = %.8f" %(Integral))
+	pyplot.plot(xt,g(xt), color = "black", label = "$g(x) = 1/2 + 3xe^{-x^2}$")
+	pyplot.fill_between(xt,g(xt), where = g(xt) >= 0, color = "green", interpolate = True, alpha = 0.3 , hatch = "///")
+	pyplot.fill_between(xt,g(xt), where = g(xt) <= 0, color = "red", interpolate = True,  alpha = 0.3 , hatch = "///")
+	pyplot.text(x = -2.0, y = 1.02, s = "Área Aproximada = %.8f" %(Integral))
 	pyplot.legend(loc = 'upper left')
-	pyplot.grid()
+	pyplot.grid(linestyle = '--', linewidth = 0.5)
 	pyplot.show()
 
 	print("\n")
-	return(print("\tA Integral Aproximada da Função é: %.8f" %(Integral)))
+	return(print("\tA Integral Aproximada da Função é: %.8f" % (Integral)))
