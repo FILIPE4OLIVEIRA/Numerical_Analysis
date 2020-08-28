854b903bdf8834e83d73a9709bb5de6594a519c2
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-08-28 19:35:20 -0300

    Update Integral_Simples_Monte_Carlo.py

--- Integral_Simples_Monte_Carlo.py
+++ Integral_Simples_Monte_Carlo.py
@@ -10,20 +10,20 @@ Created on Thu Oct 17 06:14:52 2019
 import numpy
 import matplotlib.pyplot as pyplot
 
-def g(x):
+def y(x):
 
-	return(1/2 + x*numpy.exp(-x**2))
+	return(1/2 + 3*x*numpy.exp(-x**2))
 
-def Integral_Simples(g,x0,x1):
+def Integral_Simples(y,x0=-2,x1=2):
 	random_results = []
-	random_numb = 100
+	random_numb = 1000
 	var_count = 0
 
-	while(var_count<150):
+	while(var_count<15000):
 		SOMA1 = 0
-		for x in range(random_numb):
-			X = x0+(x1-x0)*numpy.random.uniform(0,1,1)
-			SOMA1 += g(X)
+		for i in range(random_numb):
+			X = x0 + (x1-x0)*numpy.random.uniform(0,1,1)
+			SOMA1 += y(X)
 	
 		random_approximation = float((x1-x0)*(SOMA1/random_numb))
 		random_results.append(random_approximation)
@@ -33,17 +33,17 @@ def Integral_Simples(g,x0,x1):
 	Integral = float(numpy.mean(random_results))
     
     #Plotagem do Gráfico de G(x)
-	xt = numpy.linspace(x0,x1)
+	xt = numpy.linspace(x0,x1,1000)
 
 	#Plotagem do Gráfico 1
-	pyplot.title('Gráfico de g(x)')
-	pyplot.xlabel('EIXO X')
-	pyplot.ylabel('EIXO Y')
-	pyplot.plot(xt,g(xt), color = "red", label = "g(x) = 1/2 + xe^(-x²)")
-	pyplot.fill_between(xt,g(xt), color = "gray")
-	pyplot.text(x = x1*0.30, y = x1*0.10, s = "Área Aproximada = %.8f" %(Integral))
+	pyplot.figure(1)
+	pyplot.title('Gráfico de y(x)')
+	pyplot.plot(xt,y(xt), color = "black", label = "$y(x) = 1/2 + 3xe^{-x^2}$")
+	pyplot.fill_between(xt,y(xt), where = y(xt) >= 0, color = "green", interpolate = True, alpha = 0.3 , hatch = "///")
+	pyplot.fill_between(xt,y(xt), where = y(xt) <= 0, color = "red", interpolate = True,  alpha = 0.3 , hatch = "///")
+	pyplot.text(x = -2.0, y = 1.02, s = "Área Aproximada = %.8f" %(Integral))
 	pyplot.legend(loc = 'upper left')
-	pyplot.grid()
+	pyplot.grid(linestyle = '--', linewidth = 0.5)
 	pyplot.show()
 
 	#Plotagem do Gráfico 2
@@ -52,10 +52,9 @@ def Integral_Simples(g,x0,x1):
 	pyplot.hist(random_results, bins = 30, ec = 'black')
 	pyplot.xlabel('Valor Médio da Integral')
 	pyplot.ylabel('Frequência')
-	pyplot.grid()
+	pyplot.grid(linestyle = '--', linewidth = 0.5)
 	pyplot.show()
     
 	
 	print("\n")
-
 	return(print("\tA Integral Aproximada da Função é: %.8f" %(Integral)))
