aae3bd38ecb38e68f657d36022bd3913f3f5fcf8
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-08-28 19:34:09 -0300

    Update Area_Entre_Curvas.py

--- Area_Entre_Curvas.py
+++ Area_Entre_Curvas.py
@@ -11,12 +11,13 @@ import numpy
 import matplotlib.pyplot as pyplot
 
 def f(x):
-
 	return(-x**2 + 18*x -17)
 
 def g(x):
-
 	return(2*x + 5)
+
+def r(x):
+	return(f(x) - g(x))
 
 def Area_Entre_Curvas(f,g,x0=-4,x1=12):
 
@@ -42,18 +43,23 @@ def Area_Entre_Curvas(f,g,x0=-4,x1=12):
 
 	Area = Integral_F - Integral_G
 
-	#Plotagem do Gráfico de G(x)
+	#Plotagem do Gráfico
 	xt = numpy.linspace(x0,x1)
 
+	for i in range(len(xt)):
+		if(r(xt[i]) >= 0.0001):
+			X = xt[i]
+			Y = f(xt[i])
+			break
+
 	pyplot.title('Gráfico de Área entre f(x) e g(x)')
-	pyplot.xlabel('EIXO X')
-	pyplot.ylabel('EIXO Y')
-	pyplot.plot(xt,f(xt), color = "red" , label = "f(x) = -x² + 18x - 17")
-	pyplot.plot(xt,g(xt), color = "blue", label = "g(x) = 2x + 5")
-	pyplot.fill_between(xt,f(xt),g(xt), color = "gray", hatch = "/")
-	pyplot.text(x = x1*0.15, y = -x1*0.75, s = "Área Aproximada = %.8f" %(Area))
-	pyplot.legend(loc = 'upper left')
-	pyplot.grid()
+	pyplot.plot(xt,f(xt), color = "red" , label = "$f(x) = -x^2 + 18x - 17$")
+	pyplot.plot(xt,g(xt), color = "blue", label = "$g(x) = 2x + 5$")
+	pyplot.plot(X,Y, marker = ".", color = "black", markersize = 8)
+	pyplot.fill_between(xt,f(xt),g(xt), color = "green", alpha = 0.3 , hatch = "///")
+	pyplot.text(x = X + 0.65, y = Y - 7.45, s = "Área Aproximada = %.8f" %(Area))
+	pyplot.legend(loc = 'best')
+	pyplot.grid(linestyle = '--', linewidth = 0.5, drawstyle = 'steps')
 	pyplot.show()
 
 	print("\n")
