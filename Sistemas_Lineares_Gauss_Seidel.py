c691ecb1b4c412bdd3aca149d29424ed8f564e81
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2021-07-05 00:49:41 -0300

    Update Sistemas_Lineares_Gauss_Seidel.py

--- Sistemas_Lineares_Gauss_Seidel.py
+++ Sistemas_Lineares_Gauss_Seidel.py
@@ -9,37 +9,61 @@ Created on Wed Jan  1 20:30:37 2020
 
 import numpy
 
-matrix = numpy.array([[3.0,-0.1,-0.2],[0.1,7.0,-0.3],[0.3,-0.2,10.0]])
-vec_b = numpy.array([7.85,-19.3,71.4])
+A = numpy.array([[8,2,-1,0],[2,-4,0,0],[0,-2,8,-1],[0,0,-1,4]])
+b = numpy.array([1,1,1,1])
 
-def Gauss_Sidel(matrix,vec_b,max_int=100):
+def Sassenfeld(A,b):
+ 	coef = list(numpy.ones(len(b)))
+ 	for i in range(len(b)):
+ 		for j in range(len(b)):
+ 			if (i != j):
+ 				coef[i] = coef[i] + A[i][j]*coef[j]
 
-	solution  = [0,0,0]
-	vec_aprox = [0,0,0]
-	real_erro = [1,1,1]
-	erro_alvo = [0.00001,0.00001,0.00001]
+
+ 		coef[i] = coef[i]/A[i][i]
+
+ 	sf = max(coef)
+
+ 	return(sf)
+
+def Gauss_Sidel(A,b,max_int=100):
+
+	solution  = list(numpy.zeros(len(b)))
+	vec_aprox = list(numpy.zeros(len(b)))
+	real_erro = list(numpy.ones(len(b)))
+	erro_alvo = list(0.0000001*numpy.ones(len(b)))
+
+	sf = Sassenfeld(A,b)
 	
 	var_count = 0
-	while(real_erro > erro_alvo and var_count < max_int):
-		
-		for i in range(len(vec_b)):
-			SOMA1 = 0
-			for j in range(len(vec_b)):
-				if(i != j):
-					SOMA1 = SOMA1 + matrix[i,j]*vec_aprox[j]
-			vec_aprox[i] = (vec_b[i] - SOMA1)/matrix[i,i]
+	if (sf<1):
+		while(real_erro > erro_alvo and var_count < max_int):
+			
+			for i in range(len(b)):
+				SOMA1 = 0
+				for j in range(len(b)):
+					if(i != j):
+						SOMA1 = SOMA1 + A[i,j]*vec_aprox[j]
+				vec_aprox[i] = (b[i] - SOMA1)/A[i,i]
 
 
-		for k in range(len(vec_b)):
-			SOMA2 = 0
-			for h in range(len(vec_b)):
-				SOMA2 = SOMA2 + matrix[k,h]*vec_aprox[h]
-				real_erro[k] = abs(SOMA2 - vec_b[k])
+			for k in range(len(b)):
+				SOMA2 = 0
+				for h in range(len(b)):
+					SOMA2 = SOMA2 + A[k,h]*vec_aprox[h]
+					real_erro[k] = abs(SOMA2 - b[k])
 
-		var_count = var_count + 1
+			var_count = var_count + 1
 
-	solution = list(round(i,10) for i in vec_aprox)
+		iterations = var_count
+		solution = list(round(i,10) for i in vec_aprox)
 
-	print("Número de Iterações: %.i" %var_count)
+		print("Número de Iterações: %.i" %var_count)
+		print("Solução do Sistema Linear:",(solution))
 
-	return(print("Solução do Sistema Linear:",(solution)))
+	else:
+		solution  = None
+		iterations = None
+		print("Método Não Converge.")
+
+	return(solution,iterations)
