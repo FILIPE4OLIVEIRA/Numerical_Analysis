753cd81cfd0315ec5bed94b0efb77f76746dc541
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2021-07-07 13:46:57 -0300

    Update EDO_RungeKutta4_Superior.py

--- EDO_RungeKutta4_Superior.py
+++ EDO_RungeKutta4_Superior.py
@@ -4,35 +4,40 @@ Created on Wed Jan  1 19:44:36 2020
 @author: engoliveira
 
 """
+
 # EDO: y" + P(x)y' + Q(x)y = R(x)
 
-# Este método resolve o problema da EDO y" - (2/x)y' + (2/x²)y = xln(x) ; y(1) = 1 ; y'(1) = 0
+# y" - 2y' + 2y = sin(x)e^(2x)
+
+# y(0) = -0.4 ; y'(0) = -0.6
 
 # Método de Runge Kutta 4° Ordem
 import numpy
 import matplotlib.pyplot as pyplot
 
-# Função
+# Funções P(x); Q(x); R(x)
 def P(x):
-	return(-2/x)
-
+    Px = -2
+    return(Px)
+    
 def Q(x):
-	return(2/(x**2))
+    Qx = 2
+    return(Qx)
 
 def R(x):
-	return(x*numpy.log(x))
+    Rx = numpy.exp(2*x)*numpy.sin(x)
+    return(Rx)
 
 # Método de Runge Kutta 4° Ordem
-def Runge_Kutta_O4S(x0,x1,y0,y1,P,Q,R):
+def Runge_Kutta_O4S(x0,x1,y0,y1,N=50000):
 
-	numb_div = 100000
-	h = (x1-x0)/numb_div
+	h = (x1-x0)/N
 
 	x_aprox  = [x0]
 	y_aprox  = [y0]
 	dy_aprox = [y1]
 
-	for i in range(numb_div):
+	for i in range(N):
 
 		k11 = h*(dy_aprox[i])
 		k12 = h*(R(x_aprox[i]) - P(x_aprox[i])*(dy_aprox[i]) - Q(x_aprox[i])*(y_aprox[i]))
@@ -56,10 +61,9 @@ def Runge_Kutta_O4S(x0,x1,y0,y1,P,Q,R):
 	print("\n")
 	print("A função y(x) no ponto %.2f é aproximadamente: %.8f" %(x1,y_aprox[-1]))
 
+
 	# Plotagem do Gráfico 1
-	pyplot.title("Solução Numérica da EDO: y'' - (2/x)y' + (2/x²)y = xln(x)\n y(1) = 1 ; y'(1) = 0")
-	pyplot.xlabel('EIXO X')
-	pyplot.ylabel('EIXO Y')
+	pyplot.title("Solução Numérica da EDO:\n y'' - 2y' + 2y = sin(x)e^(2x) \n y(0) = -0.4 ; y'(0) = -0.6")
 	pyplot.plot(x_aprox,y_aprox, label = "Solução Númerica y(x)", color = 'blue', linestyle = 'dashed')
 	pyplot.plot(x_aprox,dy_aprox, label = "Solução Númerica y'(x)", color = 'green', linestyle = 'dashed')
 	pyplot.legend(loc = 'best')
